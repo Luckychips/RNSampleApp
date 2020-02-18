eff8b35306ecad14f8a173ab1a38c99d58504e86
todd.kim <sksms20000@gmail.com>
2020-02-18 15:15:30 +0900

    검색된 책 목록에서 pull to refresh 이벤트 추가.

--- app/screen/tabs/mypage/index.js
+++ app/screen/tabs/mypage/index.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import {StatusBar, FlatList, Dimensions} from 'react-native';
+import {StatusBar, FlatList, RefreshControl, Dimensions} from 'react-native';
 import {KAKAO_RESTAPI_KEY} from 'react-native-dotenv';
 import {Background, AdjustFitSafeArea} from '../../../components/Elements';
 import {IPHONE_X_BOTTOM_NOTCH_HEIGHT, TAB_NAVIGATION_BAR_HEIGHT} from '../../../constants';
@@ -8,6 +8,7 @@ import HiddenToBottom from './HiddenToBottom';
 const {height} = Dimensions.get('window');
 
 const MyPage = props => {
+    const [searchKeyword, setSearchKeyword] = useState('인스타그램');
     const [list, setList] = useState([]);
     const [rendered, setRendered] = useState(false);
     useEffect(() => {
@@ -25,7 +26,7 @@ const MyPage = props => {
     const retrieve = async () => {
         try {
             // title (제목에서 검색) or isbn (ISBN에서 검색) or publisher (출판사에서 검색) or person(인명에서 검색)
-            const response = await fetch('https://dapi.kakao.com/v3/search/book?target=title&query=인스타그램', {
+            const response = await fetch('https://dapi.kakao.com/v3/search/book?target=title&query=' + searchKeyword, {
                 method: 'GET',
                 headers: {
                     Accept: 'application/json',
@@ -38,6 +39,11 @@ const MyPage = props => {
         } catch (e) {
             console.log(e);
         }
+    };
+    const refresh = () => {
+        const keywords = ['심리학', '머신러닝', '영어', '세계사', '게임'];
+        setSearchKeyword(keywords[Math.floor(Math.random() * keywords.length)]);
+        setRendered(true);
     };
 
     return (
@@ -63,6 +69,13 @@ const MyPage = props => {
                             );
                         }}
                         keyExtractor={item => item.isbn}
+                        refreshControl={
+                            <RefreshControl
+                                tintColor={'#3562FF'}
+                                refreshing={rendered}
+                                onRefresh={refresh}
+                            />
+                        }
                     />
                 </AdjustFitSafeArea>
                 <HiddenToBottom />
